94ef33685d8c5c84adcd9be440bc251100049d2e
maskit-ae <maskitae@gmail.com>
2026-07-04 12:10:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const gallery = document.getElementById("gallery");
 
 for (let i = 1; i <= 131; i++) {
-    const file = images/mm${String(i).padStart(3, "0")}.JPG;
+
+    const nomor = String(i).padStart(3, "0");
+    const file = "images/mm" + nomor + ".JPG";
 
     const a = document.createElement("a");
     a.href = file;
 
     const img = document.createElement("img");
     img.src = file;
+    img.alt = "Foto " + i;
     img.loading = "lazy";
-    img.alt = Photo ${i};
 
     a.appendChild(img);
     gallery.appendChild(a);
