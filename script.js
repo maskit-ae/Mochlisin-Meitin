f98e70b6f591314c9e21c31d6c65b1b58b37b24d
maskit-ae <maskitae@gmail.com>
2026-07-04 11:43:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// reserved
+const gallery = document.getElementById("gallery");
+
+for (let i = 1; i <= 131; i++) {
+    const file = images/mm (${i}).JPG;
+
+    gallery.innerHTML += 
+        <a href="${file}">
+            <img loading="lazy" src="${file}" alt="Photo ${i}">
+        </a>
+    ;
+}
+
+lightGallery(gallery, {
+    selector: "a"
+});
