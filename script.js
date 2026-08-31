96e45d711866f7d36018412cdf37089754d125f1
maskit-ae <maskitae@gmail.com>
2026-08-31 21:37:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const gallery = document.getElementById("gallery");
+const extensions = [".JPG", ".jpg", ".Jpg", ".png"];
 
 for (let i = 1; i <= 131; i++) {
     const nomor = String(i).padStart(3, "0");
-    const file = "images/mm" + nomor + ".JPG";
-
     const img = document.createElement("img");
-    img.src = file;
     img.loading = "lazy";
     img.alt = "Foto " + i;
 
+    // Fungsi otomatis mencoba ekstensi berikutnya jika ekstensi sebelumnya gagal dimuat
+    let extIndex = 0;
+    function tryNextExtension() {
+        if (extIndex < extensions.length) {
+            img.src = "images/mm" + nomor + extensions[extIndex];
+            extIndex++;
+        }
+    }
+
+    img.onerror = function() {
+        tryNextExtension();
+    };
+
+    // Mulai coba dari ekstensi pertama
+    tryNextExtension();
     gallery.appendChild(img);
 }
